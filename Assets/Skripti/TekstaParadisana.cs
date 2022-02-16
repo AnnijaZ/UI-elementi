5e58ac704ab2baa8f0f8f7208fc84938a0c134b9
Annija <annija.zorgenberga@gmail.com>
2022-02-16 14:14:54 +0200

    Istenotas darbibas ar tekstu

---
+++ Assets/Skripti/TekstaParadisana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaParadisana : MonoBehaviour {
+	//Uzglabās tekstu, kurš paņemts no input lauka
+	public string teksts;
+	//Lauks no, kura es teksu paņemšu
+	public GameObject ievadesLauks;
+	//Teksta objekts, kur attēlot mainīgā teksts saturu
+	public GameObject tekstaAttelosanasLauks;
+
+	public void darbibasArTekstu(){
+		teksts = ievadesLauks.GetComponent<Text> ().text;
+		tekstaAttelosanasLauks.GetComponent<Text> ().text = "Sveiki " + teksts.ToUpper () + "!";
+	}
+}
